0e64336b9d7f12d6197d490982dfca760edd1014
Thibaut Coutard <thibaut@stamp.yt>
2021-12-15 08:13:05 +0100

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Thibaut Coutard on 15/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
